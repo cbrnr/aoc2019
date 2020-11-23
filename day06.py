ce2b1ebbb32d347bdad8cf1a21bf965bdc7a7c71
Clemens Brunner <clemens.brunner@gmail.com>
2020-11-23 16:13:33 +0100

    Parse orbits from string

--- day06.py
+++ day06.py
@@ -1,19 +1,26 @@
 from collections import defaultdict
 
 
-orbits = defaultdict(list)
-orbits |= {"COM": ["B"],
-           "B": ["C", "G"],
-           "G": ["H"],
-           "H": [],
-           "C": ["D"],
-           "D": ["E", "I"],
-           "I": [],
-           "E": ["J", "F"],
-           "F": [],
-           "J": ["K"],
-           "K": ["L"],
-           "L": []}
+def create_orbits(s):
+    """Create an orbit map (tree) from a string description.
+
+    Parameters
+    ----------
+    s : str
+        Orbit map defined in a string containing one item per line formatted as
+        XXX)YYY (YYY orbits XXX).
+
+    Returns
+    -------
+    orbits : dict
+        Orbit map as a tree. Each node (key) contains its children (value).
+    """
+    orbits = defaultdict(list)
+    for orbit in s.split():
+        key, value = orbit.split(")")
+        orbits[key].append(value)
+        orbits[value]  # make sure every child is created
+    return orbits
 
 
 def level(node, root, tree):
@@ -47,6 +54,18 @@ def level(node, root, tree):
             raise KeyError(f"Node '{node}' not found.")
 
 
+description = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L"""
+orbits = create_orbits(description)
 s = 0
 for planet in orbits:  # sum node levels of all nodes
     s += level(planet, "COM", orbits)
