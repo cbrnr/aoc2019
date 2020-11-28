7f4787069ae2fa863cf5edb274c8669d5728df1f
Clemens Brunner <clemens.brunner@gmail.com>
2020-11-28 11:42:35 +0100

    Day 8 part 2

--- day08.py
+++ day08.py
@@ -6,9 +6,18 @@ with open("day08.txt") as f:
 
 image = np.array([int(pixel) for pixel in file]).reshape((-1, 6, 25))
 layer = np.argmin((image == 0).sum((1, 2)))
-result = (image[layer, :, :] == 1).sum() * (image[layer, :, :] == 2).sum()
+result = (image[layer] == 1).sum() * (image[layer] == 2).sum()
 print("Part 1:", result)
 
-message = image[-1]
-for layer in range(-2, -image.shape[0], -1):
-    message[layer]
+message = np.full_like(image[0], 2)
+for layer in image:
+    for pixel in range(len(layer.flat)):
+        if message.flat[pixel] == 2 and layer.flat[pixel] != 2:
+            message.flat[pixel] = layer.flat[pixel]
+
+print("Part 2:")
+for row in message:
+    for col in row:
+        char = "\u001b[40m \u001b[0m" if col == 0 else " "
+        print(char, end="")
+    print()
