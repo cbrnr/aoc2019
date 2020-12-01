e3b5c8bb14a09939cd831656cc838ca40d326d96
Clemens Brunner <clemens.brunner@gmail.com>
2020-12-01 17:00:38 +0100

    Day 9 part 2

--- day09.py
+++ day09.py
@@ -7,4 +7,8 @@ intcode = tuple(int(i) for i in file.split(","))
 
 program = run_intcode(intcode, 1)
 for output in program:
-    print(output)
+    print("Part 1:", output)
+
+program = run_intcode(intcode, 2)
+for output in program:
+    print("Part 2:", output)
